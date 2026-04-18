959050291f7f9ed25117f873ba31bad9645aefc4
jkaps9 <jkaps9@users.noreply.github.com>
2026-04-18 21:20:06 +0000

    deploy: 31835ebbb7b267504a89426412e17566eb77f3fb

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ if (navToggle && mainNav) {
     mainNav.classList.toggle("visible");
   });
 }
+
+// Contact Form
+const contactForm = document.querySelector("#contact-form");
+const successMessage = document.querySelector(".success-message");
+
+if (contactForm) {
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (successMessage) {
+      successMessage.classList.add("visible");
+    }
+    contactForm.reset();
+    if (successMessage) {
+      setTimeout(() => successMessage.classList.remove("visible"), 5000);
+    }
+  });
+}
