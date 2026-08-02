f345097be2da75beeb73a5fa7cd634444bd9a20d
Josh <15219491+jkaps9@users.noreply.github.com>
2026-08-02 15:56:08 -0400

    update port

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run start",
-    url: "http://localhost:3000",
+    url: "http://localhost:8080",
     reuseExistingServer: !process.env.CI,
   },
 });
